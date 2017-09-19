3fecf3da886f90abab01565b9c90c59c3d2b3392
Ranieri Benvenuto <ranieribenvenuto@gmail.com>
2017-09-19 14:16:57 -0300

    Adicionando endpoint de teste

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::get('teste', function(){
+    return response("FUNCIONA ESSA PORRA MLK", 200);
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
