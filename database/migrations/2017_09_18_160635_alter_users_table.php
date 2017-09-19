9f6b3a6ea14ebf8eb68bf8bb7aa9da004b43b943
Ranieri Benvenuto <ranieribenvenuto@gmail.com>
2017-09-19 14:12:09 -0300

    Alterando o campo user_rc na table users

---
+++ database/migrations/2017_09_18_160635_alter_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table){
+            $table->string('user_rc', 9)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
